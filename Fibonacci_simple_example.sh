fb0dc070d0ff7db936abc0798479c705609b3448
sdburciaga <sdburciaga@gmail.com>
2024-09-16 13:19:56 -0600

    Adding this Fibonacci shell to my Git

---
+++ Fibonacci_simple_example.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# for loop - version 1
+for i in 0 1 1 2 3 5 8 13 21 34 55 89;
+do
+  echo "The value of variable i in version 1 of the for loop is ${i}."
+done
+
+echo "The script is finished running, goodbye!"
